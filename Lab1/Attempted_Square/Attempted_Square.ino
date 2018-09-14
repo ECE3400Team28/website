f09346d68427b70b91e691e40b6c5f9c7ff217e4
Kevin Ying <kzy2@cornell.edu>
2018-09-14 11:16:37 -0400

    Differentiate lab1 and milestone 1 code

---
+++ Lab1/Attempted_Square/Attempted_Square.ino
@@ -0,0 +1,31 @@
+#include <Servo.h>
+ Servo Motor1;
+Servo Motor2;
+ void setup() {
+  // put your setup code here, to run once:
+    int PWM1 = 3;
+    int PWM2 = 5;
+    pinMode(PWM1, OUTPUT);
+    pinMode(PWM2, OUTPUT);
+    Motor1.attach(PWM1);
+    Motor2.attach(PWM2);
+}
+ void loop() {
+  // put your main code here, to run repeatedly:
+    Motor1.write(180);
+    Motor2.write(0);
+    delay(1200);
+    Motor1.write(90);
+    Motor2.write(90);
+    delay(600);
+    turn();
+}
+ void turn(){
+    Motor1.write(180);
+    Motor2.write(180);
+    delay(350);
+    Motor1.write(90);
+    Motor2.write(90);
+    delay(250);
+    return;
+}
