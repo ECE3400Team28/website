891dc2f44544b8341080c81a5c050d1dc87fc25d
Glenna Zhang <glz6@cornell.edu>
2018-09-17 22:38:18 -0400

    added files for audio team

---
+++ Lab2/fft_adc_serial/fft_adc_serial.ino
@@ -0,0 +1,38 @@
+/*
+fft_adc_serial.pde
+guest openmusiclabs.com 7.7.14
+example sketch for testing the fft library.
+it takes in data on ADC0 (Analog0) and processes them
+with the fft. the data is sent out over the serial
+port at 115.2kb.
+*/
+
+#define LOG_OUT 1 // use the log output function
+#define FFT_N 256 // set to 256 point fft
+
+#include <FFT.h> // include the library
+
+void setup() {
+  Serial.begin(115200); // use the serial port
+}
+
+void loop() {
+  while(1) { // reduces jitter
+    cli();  // UDRE interrupt slows this way down on arduino1.0
+    for (int i = 0 ; i < 512 ; i += 2) { // save 256 samples
+      fft_input[i] = analogRead(A0); // put real data into even bins
+      fft_input[i+1] = 0; // set odd bins to 0
+    }
+    fft_window(); // window the data for better frequency response
+    fft_reorder(); // reorder the data before doing the fft
+    fft_run(); // process the data in the fft
+    fft_mag_log(); // take the output of the fft
+    sei();
+    Serial.println("start");
+    String out = "";
+    for (byte i = 0 ; i < FFT_N/2 ; i++) { 
+      Serial.println(out + fft_log_out[i] + " " + i); //send out data
+    }
+    while(1) {}
+  }
+}
