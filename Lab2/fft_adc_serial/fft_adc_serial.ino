1f7a44f0591ce585d12527e7451ff08f661ca052
Glenna Zhang <glz6@cornell.edu>
2018-10-01 23:58:10 -0400

    added a comment

--- Lab2/fft_adc_serial/fft_adc_serial.ino
+++ Lab2/fft_adc_serial/fft_adc_serial.ino
@@ -17,22 +17,20 @@ void setup() {
 }
 
 void loop() {
-  while(1) { // reduces jitter
-    cli();  // UDRE interrupt slows this way down on arduino1.0
-    for (int i = 0 ; i < 512 ; i += 2) { // save 256 samples
-      fft_input[i] = analogRead(A0); // put real data into even bins
-      fft_input[i+1] = 0; // set odd bins to 0
-    }
-    fft_window(); // window the data for better frequency response
-    fft_reorder(); // reorder the data before doing the fft
-    fft_run(); // process the data in the fft
-    fft_mag_log(); // take the output of the fft
-    sei();
-    Serial.println("start");
-    String out = "";
-    for (byte i = 0 ; i < FFT_N/2 ; i++) { 
-      Serial.println(out + fft_log_out[i] + " " + i); //send out data
-    }
-    while(1) {}
+  cli();  // UDRE interrupt slows this way down on arduino1.0
+  for (int i = 0 ; i < 512 ; i += 2) { // save 256 samples
+    fft_input[i] = analogRead(A0); // put real data into even bins
+    fft_input[i+1] = 0; // set odd bins to 0
   }
+  fft_window(); // window the data for better frequency response
+  fft_reorder(); // reorder the data before doing the fft
+  fft_run(); // process the data in the fft
+  fft_mag_log(); // take the output of the fft
+  sei();
+  Serial.println("start");
+  String out = "";
+  for (byte i = 0 ; i < FFT_N/2 ; i++) { 
+    Serial.println(out + fft_log_out[i] + " " + i); //send out data
+  }
+  while(1) {} // pause here to show only one FFT result
 }
